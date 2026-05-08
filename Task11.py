25a562c34d3a0a24542873f36c28adaa0f6a23c1
shauxzxcccv <alex.kysiliov@gmail.com>
2026-05-08 17:20:58 +0200

    deploy

--- Task11.py
+++ Task11.py
@@ -10,8 +10,7 @@ while True:
         break
 
     elif main == "add":
-
-        while True:
+            
             name = input("\nEnter the name of the country: \n")
             code = input("\nEnter the country code: \n")
             population = input("\nEnter the population: \n")
@@ -20,7 +19,7 @@ while True:
 
             country = {
                 "name": name,
-                "code": code,
+                "code": code,                                   # -- as "code" user can use numerical prefixes of the countries(+48, +49, +1) or three-letter abbreviations(POL, GER, USA)
                 "population": population,
                 "location": location,
                 "president": president
@@ -28,7 +27,7 @@ while True:
 
             countries.append(country)
             print("\nCountry added successfully!\n")
-            break
+            continue
 
     elif main != "add" and main != "view" and main != "exit":
         print("\nInvalid input. Please enter 'add', 'view', or 'exit'.\n")
@@ -38,36 +37,41 @@ while True:
         if not countries:
             print("\nNo countries added yet. Please add a country first.\n")
             continue
+
         elif main == "view":
+
             print("\nAvailable countries:")
+
             for country in countries:
                 print("- " + country["name"])
-                choice = input("\nWhich country do you want to know about? (Enter the name of the country): \n")
-                if choice in [country["name"] for country in countries]:
-                    selected_country = next(country for country in countries if country["name"] == choice)
-                    
-                    while True:
-    
-                        info = input("\nEnter the information you want to know (code/population/location/president): \n")
-    
-                        if info in selected_country:
-                            print("\n" + "Answer: " + str(selected_country[info]) + "\n")
-                            break
-                        
-                        else:
-                            print("\nInvalid information. Please enter one of the following: (code, population, location, president).\n")
-                            continue
+                continue
+    while True: 
+        choice = input("\nType name of the country you want to view, or go back to the main menu: (Enter the name of country/main menu) \n")
+        
+        if choice in [country["name"] for country in countries]:
+                
+                selected_country = next(country for country in countries if country["name"] == choice)
+                for key, value in selected_country.items():
+                    print(f"{key:<10} | {value}")
+                    continue
+
+#                while True:                                                                                                                #
+#                                                                                                                                           #
+#                        info = input("\nEnter the information you want to know (code/population/location/president): \n")                  #
+#                                                                                                                                           #
+#                        if info in selected_country:                                                                                       #
+#                            print("\n" + "Answer: " + str(selected_country[info]) + "\n")                                                  #           -- if you want to use special search via keys 
+#                            break                                                                                                          #              to find more specific information
+#                                                                                                                                           #
+#                        else:                                                                                                              #
+#                            print("\nInvalid information. Please enter one of the following: (code, population, location, president).\n")  #
+#                            continue                                                                                                       #
+
+        elif choice == "main menu":
+             break
+        
+        else:
+             print("\n Invalid answer. Please, try again.\n")
+             continue
 
 
-#while True:
-#    if main == "view":
-#        choice = input("\nWhich country do you want to know about? (Enter the name of the country): \n")
-#        if choice in [country["name"] for country in countries]:
-#            selected_country = next(country for country in countries if country["name"] == choice)
-#            info = input("\nEnter the information you want to know (code/population/location/president): \n")
-#            if info in selected_country:
-#                print("\n" + "Answer: " + str(selected_country[info]) + "\n")
-#                break
-#            else:
-#                print("\nInvalid information. Please enter one of the following: (code, population, location, president).\n")
-#                continue
